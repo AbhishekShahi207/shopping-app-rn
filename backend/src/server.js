4d3f915e8bf53e7b515d0319ddcfc39c45563979
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-02-14 21:28:56 +0530

    config changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,12 +10,12 @@ app.get("/api/health",(req,res)=>{
 })
 
 //deployement 
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../admin/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
 
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../admin","dist","index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
 }
 
 app.listen(ENV.PORT,()=>{
