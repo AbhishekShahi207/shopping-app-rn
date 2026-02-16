d68f895dec13e6796aa4ce1d3cdacaf2063f9757
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-02-16 13:14:57 +0530

    db,cloudinary config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import path from "path"
+import { clerkMiddleware } from '@clerk/express'
 import { ENV } from "./config/env.js"
+import { connectDb } from "./config/db.js"
 
 const app=express()
 const __dirname=path.resolve()
+
+app.use(clerkMiddleware()) //it adds auth object to every req
 
 app.get("/api/health",(req,res)=>{
     res.json({message:"App is rung "})
@@ -19,5 +23,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(ENV.PORT,()=>{
+  connectDb()
     console.log("Server is Runing")
 })
