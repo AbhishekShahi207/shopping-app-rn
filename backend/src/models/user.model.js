3d51ba47ba6267ce246e3f954bc7fbec31e19586
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-02-17 12:44:13 +0530

    inngest webhook implementation

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose"
+
+const addressSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  isDefault: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+
+const userSchema= new mongoose.Schema({
+   email:{
+    type:String,
+    required:true,
+    unique:true
+   },
+   name:{
+    required:true,
+    type:String,
+   },
+   imageUrl:{
+    type:String,
+    default:""
+   },
+   clerkId:{
+    required:true,
+    type:String,
+    unique:true,
+   },
+   addresses:[addressSchema],
+wishlist:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Product"
+}]
+},{timestamps:true})
+
+export const User=mongoose.model("User",userSchema)
